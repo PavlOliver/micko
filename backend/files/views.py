ee87d10480eb7ab6f3d1fb3af6710c7b26732391
oliverPavlanin <oliverpavlanin@gmail.com>
2024-12-15 18:31:32 +0100

    zobrazenie alergii v zdravotnej karte

--- backend/files/views.py
+++ backend/files/views.py
@@ -2,7 +2,6 @@ import os
 from datetime import timedelta
 from io import BytesIO
 
-from dns.e164 import query
 from flask import Blueprint, jsonify, request, send_file, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_required
@@ -49,7 +48,8 @@ def orders():
         new_order = insert_new_order(request.json['reason'], request.json['patient'], request.json['doctor'],
                                      request.json['room'],
                                      request.json['blocks'], request.json['date'], request.json['time'])
-        return jsonify({'last_order': new_order.to_dic()}) if new_order.datum_objednavky.isocalendar().week == datetime.now().isocalendar().week else jsonify(
+        return jsonify({
+                           'last_order': new_order.to_dic()}) if new_order.datum_objednavky.isocalendar().week == datetime.now().isocalendar().week else jsonify(
             {'message': 'Order created'})
     elif request.method == 'PUT':
         edited_order = update_order(request.json['id'], request.json['reason'], request.json['patient'],
@@ -260,11 +260,6 @@ def get_zdravotna_karta(id_poistenca):
         zdrav_zaznamy = ZdravotnyZaznam.query.filter_by(pacient=id_poistenca).all()
         zdrav_zaznamy = [z.to_vysledok_vysetrenia() for z in zdrav_zaznamy]
         rec = [r.zaznam() for r in rec]
-        print(hospitalizacie)
-        print('meno:', osoba.to_dic())
-        print (zdrav_zaznamy)
-
-
         if pacient:
             datum_narodenia = extract_date_of_birth(pacient.rod_cislo)
             zdravotna_karta = {
@@ -277,7 +272,7 @@ def get_zdravotna_karta(id_poistenca):
                 'hospitalizacie': hospitalizacie,
                 'vysledkyVysetreni': zdrav_zaznamy,
                 'recepty': rec,
-                'alergie': pacient.alergie()
+                'alergie': pacient.alergie
             }
             return jsonify({'zdravotna_karta': zdravotna_karta, 'username': select_current_user().login})
         return jsonify({'error': 'Pacient not found'})
@@ -347,6 +342,7 @@ def get_zamestnanci():
     except Exception as e:
         print("Error in get_zamestnanci:", str(e))
         return jsonify({'error': str(e)}), 500
+
 
 @views.route('/room_list', methods=['GET'])
 @login_required
